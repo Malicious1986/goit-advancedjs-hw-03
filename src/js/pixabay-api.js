7a49e51091d4a4a65ee3881d9bb090e5bbb7f9b4
Osadchyi Yurii <malicious1986@gmail.com>
2025-05-18 21:31:59 +0100

    done

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://pixabay.com/api/';
+
+function getParams(query) {
+  const params = new URLSearchParams({
+    key: '50351330-2fc0fc2b1e2442bcaa307ce6b',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return '?' + params.toString();
+}
+
+export function getPictures(query) {
+  const url = BASE_URL + getParams(query);
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.error(error));
+}
